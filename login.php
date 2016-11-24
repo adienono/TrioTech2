26dee76bd822ee67f0217936ab432fd9661e905f
adienono <adienono@gmail.com>
2016-11-24 09:39:57 -0500

    new files and text added

---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel = "stylesheet" href ="logStyle.css">
+</head> 
+<body>
+<h1>  Login Page</h1>
+<form action = "" method ="post">
+Username: <input type="text" name= "username"><br/><br/>
+Password: <input type="text" name="password"><br/><br/>
+<input type="submit" name="submit" value="Login">
+</form>
+
+</body>
+
+
+</html>
