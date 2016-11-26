d1f0c744a312060a917805319252ecb9a5ab36fb
adienono <adienono@gmail.com>
2016-11-26 17:41:03 -0500

    hidden password code to login

--- index.php
+++ index.php
@@ -1,6 +1,71 @@
-<?php
-echo "Our php page!";
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" href ="style.css"/>
+    <a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">☰</a>
+    <title>TiroTech!</title>
+    
+    <style>
+       
+     /* I may decide to syle this in css instead */ 
+    /* lets see how is works out */ 
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 200px;
+    background-color: #f1f1f1;
+  
+}
+  /* Change the link color on hover  just trying out the command*/
+li a:hover {
+    background-color: #555;
+    color: white;
+}
+li a {
+    display: block;
+    color: #000;
+    padding: 8px 16px;
+    text-decoration: none;
+}     
+      
+  </style>
+  
+  </head>
+       
+  <body>
+    // add logo image an the link below 
+    <img src="logo image"/>
+    <div class="header"><h1>
+      
+      <?php
+      $welcome = "GAMING & SURFING IN STYLE!";
+      echo $welcome;
+      
+ <ul class="nav" id="nav">
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#products">Products</a></li>
+  <li><a href="#gaming">Gaming</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+  </li>
+</ul>
+<script>
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+</script>
 
+      ?>
+      
 
-
-?>
+   
+  </body>
+</html>
--- login.php
+++ login.php
@@ -3,10 +3,13 @@
 <link rel = "stylesheet" href ="logStyle.css">
 </head> 
 <body>
+  
 <h1>  Login Page</h1>
+  
+  
 <form action = "" method ="post">
 Username: <input type="text" name= "username"><br/><br/>
-Password: <input type="text" name="password"><br/><br/>
+Password: <input type="password" name="password"><br/><br/>
 <input type="submit" name="submit" value="Login">
 </form>
 
